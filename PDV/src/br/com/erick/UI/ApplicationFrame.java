e47657d84a6915f0bea5e88a0c92fc434d5823af
Erick Andrade <erickspace11@hotmail.com>
2025-10-06 14:13:33 -0300

    build

---
+++ PDV/src/br/com/erick/UI/ApplicationFrame.java
@@ -0,0 +1,4 @@
+package br.com.erick.UI;
+
+public class ApplicationFrame {
+}
---
+++ PDV/src/br/com/erick/produtos/Produto.java
@@ -0,0 +1,10 @@
+package br.com.erick.produtos;
+
+public class Produtos {
+    private String SKU;
+    private String nome;
+    private double preco;
+    private int estoque;
+
+
+    public Produto
